5f818aada26a63e63c7782fb27a7517d14025976
André Borges <borges.an@hotmail.com>
2023-03-19 20:18:04 -0300

    up code

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,4 +1,4 @@
-import { api } from '@/src/lib/axios'
+import { api } from '../../lib/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { useRouter } from 'next/router'
