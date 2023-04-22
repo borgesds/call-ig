40cd2cfa133ff9f25fa167ecea86c2cca57b2855
André Fonseca Borges <borges.an@hotmail.com>
2023-04-22 14:28:13 -0300

    up Code

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,8 +1,25 @@
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 import { ConfirmForm, FormActions, FormHeader } from './styles'
 import { CalendarBlank, Clock } from 'phosphor-react'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const confirmFormSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  observations: z.string().nullable(),
+})
 
 export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    resolver: zodResolver(confirmFormSchema),
+  })
+
   function handleConfirmScheduling() {}
   return (
     <ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
@@ -20,17 +37,21 @@ export function ConfirmStep() {
 
       <label>
         <Text size="sm">Nome Completo</Text>
-        <TextInput type="text" placeholder="Seu nome" />
+        <TextInput type="text" placeholder="Seu nome" {...register('name')} />
       </label>
 
       <label>
         <Text size="sm">Endreço de e-mail</Text>
-        <TextInput type="email" placeholder="johndone@example.com" />
+        <TextInput
+          type="email"
+          placeholder="johndone@example.com"
+          {...register('email')}
+        />
       </label>
 
       <label>
         <Text size="sm">Observações</Text>
-        <TextArea />
+        <TextArea {...register('observations')}/>
       </label>
 
       <FormActions>
