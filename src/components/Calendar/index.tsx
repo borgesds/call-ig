6b27920bd5a95cee7ae58e77a52db1520462dd1d
André Fonseca Borges <borges.an@hotmail.com>
2023-04-24 07:32:46 -0300

    up Code

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -9,7 +9,7 @@ import {
   Division,
 } from './styles'
 import { getWeekDays } from '@/src/utils/get-week-days'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 
 export function Calendar() {
@@ -17,11 +17,47 @@ export function Calendar() {
     return dayjs().set('date', 1)
   })
 
+  // change the months using the < > button
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  // change the months using the < > button
+  function handleNextMonth() {
+    const previousMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
 
   const currentMonth = currentDate.format('MMMM')
-
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    // get the day of the week, ex: Friday
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
@@ -31,11 +67,11 @@ export function Calendar() {
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous moth">
             <CaretLeft />
           </button>
 
-          <button>
+          <button onClick={handleNextMonth} title="Next moth">
             <CaretRight />
           </button>
         </CalendarActions>
