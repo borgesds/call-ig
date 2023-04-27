97f12b13bfffb4a454f44389717f57d7b3f745f6
André Fonseca Borges <borges.an@hotmail.com>
2023-04-27 07:11:48 -0300

    up Code

---
+++ src/pages/api/users/[username]]/avaliability.api.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided'})
+  }
+}
