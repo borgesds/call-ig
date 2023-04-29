50dcac6310fffdb14d4a1f1237d7bd8c9aa4be07
André Fonseca Borges <borges.an@hotmail.com>
2023-04-28 21:16:34 -0300

    up Code

--- src/pages/api/users/[username]]/avaliability.api.ts
+++ src/pages/api/users/[username]]/avaliability.api.ts
@@ -1,3 +1,5 @@
+import { prisma } from '@/src/lib/prisma'
+import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handle(
@@ -12,6 +14,30 @@ export default async function handle(
   const { date } = req.query
 
   if (!date) {
-    return res.status(400).json({ message: 'Date not provided'})
+    return res.status(400).json({ message: 'Date not provided.' })
   }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day')
+    },
+  })
 }
