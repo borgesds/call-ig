d297d0b1b7fae1f3d63c2499eb13e38a63dff0e6
André Borges <borges.an@hotmail.com>
2023-05-01 13:20:31 -0300

    up Code

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,4 +1,3 @@
-import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
 
-export const queryClient = new QueryClientProvider()
-==>
+export const queryClient = new QueryClient()
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -10,6 +10,7 @@ import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
 import { api } from '@/src/lib/axios'
+import { useQuery } from '@tanstack/react-query'
 
 interface Availability {
   possibleTimes: number[]
@@ -18,7 +19,7 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState<Availability | null>(null)
+  /*   const [availability, setAvailability] = useState<Availability | null>(null) */
 
   const router = useRouter()
 
@@ -31,7 +32,27 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format('DD[ de ]MMMM')
     : null
 
-  useEffect(() => {
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
+
+  const { data: availability } = useQuery<Availability>(
+    ['availability', selectedDateWithoutTime],
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
+        params: {
+          date: selectedDateWithoutTime,
+        },
+      })
+
+      return response.data
+    },
+    {
+      enabled: !!selectedDate,
+    },
+  )
+
+  /*   useEffect(() => {
     if (!selectedDate) {
       return
     }
@@ -45,7 +66,7 @@ export function CalendarStep() {
       .then((response) => {
         setAvailability(response.data)
       })
-  }, [selectedDate, username])
+  }, [selectedDate, username]) */
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
