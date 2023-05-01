090129be2b52505ac2ff141630909312a36b30d8
André Borges <borges.an@hotmail.com>
2023-05-01 13:35:15 -0300

    up Code

---
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,31 @@
+import { prisma } from '@/src/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided.' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  return res.json({  })
+}
