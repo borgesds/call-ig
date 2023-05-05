5a802a5bcdf464407bd491166b455aa8ccace89f
André Borges <borges.an@hotmail.com>
2023-05-05 07:23:14 -0300

    up Code

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -13,6 +13,7 @@ import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 import { useQuery } from '@tanstack/react-query'
 import { useRouter } from 'next/router'
+import { api } from '@/src/lib/axios'
 
 interface CalendarWeek {
   week: number
@@ -76,6 +77,10 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   )
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
+
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
@@ -115,7 +120,7 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
           date,
           disabled:
             date.endOf('day').isBefore(new Date()) ||
-            blockedDates?.blockedWeekDays.includes(date.get('day')),
+            blockedDates.blockedWeekDays.includes(date.get('day')),
         }
       }),
       ...nextMonthFillArray.map((date) => {
